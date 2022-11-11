9c2ffe3887f8019b8be2585bb05cae0424d648c9
kattchh <kattch650@gmail.com>
2022-11-11 00:40:12 -0500

    first!

---
+++ jstest.js
@@ -0,0 +1,15 @@
+//function dogpuppies() {
+  //  let box = document.querySelector(".container");
+  //  box.classList.toggle("newClass1");
+  //  console.log)"this totally works");
+
+  //}
+
+
+  function darkmode() {
+      let variable1 = document.querySelector(".container");
+      let variable2 = document.querySelector(".container");
+      variable1.classList.toggle("darkmodeContainer");
+      variable2.classList.toggle("darkmodeContainer");
+
+    }
